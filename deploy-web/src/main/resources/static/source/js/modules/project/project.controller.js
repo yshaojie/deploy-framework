94e8080afa1f929e6d4b9b592f835e2031f0eeac
shaojieyue <yuesj@huatu.com>
2017-02-21 18:44:37 +0800

    初始化工程

--- deploy-web/src/main/resources/static/source/js/modules/project/project.controller.js
+++ deploy-web/src/main/resources/static/source/js/modules/project/project.controller.js
@@ -8,15 +8,23 @@
     ProjectController.$inject = ['ProjectService','$scope'];
     function ProjectController(ProjectService,$scope) {
         var vm = this;
+        $scope.loadProject = loadProject;
         active();
         function active() {
 
             ProjectService.findProjectList(function (data) {
                 $scope.projectList=data;
-                $scope.projectList=data;
+                $scope.project = $scope.projectList[0]
             })
 
 
+        }
+
+        $scope.project = {
+            url:"http://xxx"
+        }
+        function loadProject(index) {
+            $scope.project = $scope.projectList[index];
         }
 
     }
