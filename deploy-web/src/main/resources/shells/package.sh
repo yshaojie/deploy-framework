94b4fc68cc08ed91a0ef2565862fc9a53012e8f3
shaojieyue <yuesj@huatu.com>
2017-02-24 08:47:56 +0800

    完善代码

--- deploy-web/src/main/resources/shells/package.sh
+++ deploy-web/src/main/resources/shells/package.sh
@@ -4,23 +4,23 @@ start_time=`date +%s`
 package_name="${module}-dist.zip"
 package_dir="${project}/${module}/target"
 git_branch=${branch}
-env_package_cmd="mvn  clean package -U -Dmaven.test.skip=true -Dmaven.compile.fork=true -pl ${module} -am "
-echo "${env_package_cmd}"
+package_cmd="mvn  clean package -U -Dmaven.test.skip=true -Dmaven.compile.fork=true -pl ${module} -am "
+echo "${package_cmd}"
 #base="/tmp"
 base="/data/deploy"
-build=${base}/projects
+projects_root=${base}/projects
 
 link_path="${base}/dist-source/"
 
 ########################################################
 
 #develop env
-rm -rf ${build}/${project}
-cd ${build}
+rm -rf ${projects_root}/${project}
+cd ${projects_root}
 git clone ${project_git_path} -b ${git_branch}
-cd ${build}/${project}
+cd ${projects_root}/${project}
 #mvn execute
-${env_package_cmd}
+${package_cmd}
 cd ${module}
 if [ ! -f target/${module}-*-dist.zip ];  then
     echo "error:no such file target/${module}-*-dist.zip package fail"
